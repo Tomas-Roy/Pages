85ae6534d31e35923e57537274770ab190f5a334
Tomas Roy <tomasroy.info@gmail.com>
2024-07-17 21:28:43 +0600

    single page modal
    single page modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,8 @@
 
-$('.one-carousel').owlCarousel({
-    loop:true,
-    margin:45,
-    nav:false,
-    autoplay:true,
-    autoplayTimeout:3000,
-    autoplayHoverPause:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:2
-        },
-        992:{
-            items:2
-        },
-        1200:{
-            items:3
-        }
-    }
-})
-
-
-$('.two-carousel').owlCarousel({
-  loop:true,
-  margin:45,
-  nav:true,
-  autoplay:true,
-  autoplayTimeout:2000,
-  autoplayHoverPause:true,
-  responsive:{
-    0:{
-        items:1
-    },
-    600:{
-        items:2
-    },
-    992:{
-        items:2
-    },
-    1200:{
-        items:5
-    }
-  }
-})
-
 
 $(document).ready(function(){
+
+  
 
 
   $('.show').click(function(){
@@ -63,21 +18,6 @@ $(document).ready(function(){
 
 
 })
-
-
-
-const items = document.querySelectorAll(".accordion button");
-
-function toggleAccordion() {
-  const itemToggle = this.getAttribute('aria-expanded');
-  
-  for (i = 0; i < items.length; i++) {
-    items[i].setAttribute('aria-expanded', 'false');
-  }
-  
-  if (itemToggle == 'false') {
-    this.setAttribute('aria-expanded', 'true');
-  }
-}
-
-items.forEach(item => item.addEventListener('click', toggleAccordion));
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})
